79cb10f8ddf8c9d87b288b781c1cda25f2a9e9ce
rajputdishika <59726841+rajputdishika@users.noreply.github.com>
2020-01-24 23:04:51 +0530

    complete code of perm_and_comb using recursion

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -1,17 +1,65 @@
 #include<iostream>
 using namespace std;
 
-int permutation(int n, int r)
+float permutation(int n, int r)
 {
+  if(n==0||r==0)
+  return 1;
+  else
+  {
+    if(n!=0)
+    {
+      return (1.0*n*pemutation(n-1,r-1));
+     }
+  }
   return 0;
 }
-
-int combination(int n, int r)
+float combination(int n, int r)
 {
+  if(n==0||r==0)
+  return 1;
+  else
+  {
+     if(n!=0)
+     {
+       return (1.0*n*combination(n-1,r-1)/r);
+     }
+  }
   return 0;
 }
 
 int main()
 {
+  int n,r;
+  cout<<"enter the value of n";
+  cin>>n;
+  cout<<"enter the value of r";
+  cin>>r;
+  try
+  {
+    
+    if(n<0||r<0)
+    {
+      throw 5;
+    }
+    
+    else if(r>n)
+    {
+      throw 5;
+    }
+    else
+    {
+      float permu=permutation(n,r);
+      cout<<"permutation of given number :"<<permu;
+      float combi=combination(n,r);
+      cout<<"combination of given number :"<<combi;
+    }
+    
+  }
+  catch(int x)
+  {
+    cout<<"cannot be calculated...enter the positive number ";
+  }
+  
   return 0;
 }
